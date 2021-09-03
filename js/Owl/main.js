e88c1d433e2e0b06565def072d5a0076f617e54a
PabloDantasDev <pabloj72017@gmail.com>
2021-09-03 19:31:15 -0300

    Novas Funcionalidades Adicionadas
    :white_check_mark:  Animação de Banner Principal.
    :white_check_mark: Animação de Hover Em Items de Lista.
    :white_check_mark: Criação de Model Para Mais informações.

--- js/Owl/main.js
+++ js/Owl/main.js
@@ -1,5 +1,5 @@
 $('.owl-carousel').owlCarousel({
-    loop:false,
+    loop:true,
     margin:10,
     nav:false,
     responsive:{
@@ -38,3 +38,49 @@ function abrirPopup(url,w,h) {
     newwindow.focus();
     return false;
     }
+
+
+    
+
+    /*-----SCRIPT SHOW MODAL----*/ 
+    function showModal(){
+        var element = document.getElementById("modal");
+        element.classList.
+        add("show-modal");
+    }
+
+    /*SCRIPT HIDEEN MODAL*/ 
+    function hideModal(){
+        var element = document.getElementById("modal");
+        element.classList.remove("show-modal")
+    }
+
+
+     /*-----SCRIPT SHOW MODAL----*/ 
+     function showModalILHA(){
+        var element = document.getElementById("modalILHA");
+        element.classList.
+        add("show-modalILHA");
+    }
+
+    /*SCRIPT HIDEEN MODAL*/ 
+    function hideModalILHA(){
+        var element = document.getElementById("modalILHA");
+        element.classList.remove("show-modalILHA")
+    }
+
+
+     /*-----SCRIPT SHOW MODAL----*/ 
+     function showModalLOKI(){
+        var element = document.getElementById("modalLOKI");
+        element.classList.
+        add("show-modalLOKI");
+    }
+
+    /*SCRIPT HIDEEN MODAL*/ 
+    function hideModalLOKI(){
+        var element = document.getElementById("modalLOKI");
+        element.classList.remove("show-modalLOKI")
+    }
+
+    
